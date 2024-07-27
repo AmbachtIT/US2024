b3b14f5e9992dc79ae3dacf053e1212805e75de4
AmbachtIT <34813625+AmbachtIT@users.noreply.github.com>
2024-07-27 23:59:41 +0200

    Added geo json data for states

--- src/D66.US2024/States.cs
+++ src/D66.US2024/States.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO.Converters;
 
 namespace D66.US2024
 {
@@ -15,7 +19,7 @@ namespace D66.US2024
 		/// Reads the state data from the assembly
 		/// </summary>
 		/// <returns></returns>
-		public static IEnumerable<State> Read()
+		public static IEnumerable<State> ReadData()
 		{
 			var type = typeof(States);
 			using var stream = type.Assembly.GetManifestResourceStream(type, "Data.States.txt");
@@ -23,6 +27,22 @@ namespace D66.US2024
 			using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
 			return csv.GetRecords<State>().ToList();
+		}
+
+
+		public static FeatureCollection ReadShapes()
+		{
+			var type = typeof(States);
+			using var stream = type.Assembly.GetManifestResourceStream(type, "Data.States.geo.json");
+			using var reader = new StreamReader(stream);
+
+			var options = new JsonSerializerOptions();
+			options.Converters.Add(new GeoJsonConverterFactory());
+
+			
+
+			var json = reader.ReadToEnd();
+			return JsonSerializer.Deserialize<FeatureCollection>(json, options)!;
 		}
 
 	}
--- test/D66.US2024.Test/TestStates.cs
+++ test/D66.US2024.Test/TestStates.cs
@@ -1,3 +1,5 @@
+using NetTopologySuite.Features;
+
 namespace D66.US2024.Test
 {
 
@@ -6,15 +8,48 @@ namespace D66.US2024.Test
 	{
 
 		[Test()]
-		public void ReadStates()
+		public void ReadStateData()
 		{
-			var states = States.Read().ToList();
-			Assert.AreEqual(51, states.Count); // 50 states + DC
+			var data = States.ReadData().ToList();
+			Assert.AreEqual(51, data.Count); // 50 states + DC
 
-			var totalElectoralVotes = states.Sum(s => s.ElectoralVotes);
+			var totalElectoralVotes = data.Sum(s => s.ElectoralVotes);
 
 			Assert.AreEqual(538, totalElectoralVotes);
 		}
+
+		[Test()]
+		public void ReadStateShapes()
+		{
+			var shapes = States.ReadShapes();
+			
+			Assert.AreEqual(51, shapes.Count); // 50 states + DC
+		}
+
+
+		[Test(), TestCaseSource(nameof(AllData))]
+		public void DataShouldHaveShape(State state)
+		{
+			var shape = _shapes.FirstOrDefault(s => s.GetOptionalId("id")?.ToString() == state.Code);
+			Assert.NotNull(shape);
+		}
+
+
+		[Test(), TestCaseSource(nameof(AllShapes))]
+		public void ShapeShouldHaveData(IFeature state)
+		{
+			var id = state.GetOptionalId("id")?.ToString();
+			var data = _data.FirstOrDefault(s => s.Code == id);
+			Assert.NotNull(data);
+		}
+
+
+		private static IEnumerable<State> AllData() => _data;
+		private static IEnumerable<IFeature> AllShapes() => _shapes;
+
+
+		private static readonly List<State> _data = States.ReadData().ToList();
+		private static readonly FeatureCollection _shapes = States.ReadShapes();
 
 	}
 }
