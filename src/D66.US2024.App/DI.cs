bc506670bca059742190feaa0d7216510d1678d1
AmbachtIT <34813625+AmbachtIT@users.noreply.github.com>
2024-07-28 09:43:18 +0200

    Added MudBlazor

---
+++ src/D66.US2024.App/DI.cs
@@ -0,0 +1,21 @@
+﻿using Ambacht.Common;
+using Ambacht.Common.Blazor;
+using Ambacht.Common.Maps;
+using MudBlazor.Services;
+
+namespace D66.US2024.App
+{
+	public static class DI
+	{
+
+		public static void AddUS2024App(this IServiceCollection services, string baseAddress)
+		{
+			services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+			services.AddAmbachtCommon();
+			services.AddAmbachtCommonMaps();
+			services.AddAmbachtCommonBlazor(baseAddress);
+			services.AddMudServices();
+		}
+
+	}
+}
--- src/D66.US2024.App/Program.cs
+++ src/D66.US2024.App/Program.cs
@@ -11,7 +11,9 @@ namespace D66.US2024.App
 			builder.RootComponents.Add<App>("#app");
 			builder.RootComponents.Add<HeadOutlet>("head::after");
 
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddUS2024App(builder.HostEnvironment.BaseAddress);
+
+
 
 			await builder.Build().RunAsync();
 		}
