6b161d3f0c266badaabe8748e27a60d29569255c
AmbachtIT <34813625+AmbachtIT@users.noreply.github.com>
2024-07-30 11:44:52 +0200

    Use SVG for bar charts to fix issues on some browsers

--- src/D66.US2024.App/CssVersion.cs
+++ src/D66.US2024.App/CssVersion.cs
@@ -2,5 +2,5 @@ namespace D66.US2024.App;
 
 public static class CssVersion
 {
-	public const string Value = "7a15c3aa-112e-46af-9225-93a92a9ff60d";
+	public const string Value = "65fc15c4-0bcf-4463-bbef-d27b76f0a3c2";
 }
